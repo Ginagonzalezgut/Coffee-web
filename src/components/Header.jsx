d325ff7f14429a07ec89cf0ef07b9803db06e41a
Gina González <gini_go@hotmail.com>
2024-09-28 14:54:34 +0200

    fix link logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../scss/components/Header.scss";
 import logo from "../images/logo2.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <>
       <header className="header">
-        <img className="header__image" src={logo} alt="Coffee Web" />
+        <Link to="/">
+          <img className="header__image" src={logo} alt="Hangoutspots" />
+        </Link>
+
         <div className="header__links">
           <a className="header__login" href="">
             Iniciar Sesión
