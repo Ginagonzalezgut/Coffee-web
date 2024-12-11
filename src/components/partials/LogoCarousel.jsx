bcf1924070cb0e15a1427cb73c0e1678db10138b
Gina González <gini_go@hotmail.com>
2024-12-11 13:30:20 +0100

    refacor renderLogos

--- src/components/partials/LogoCarousel.jsx
+++ src/components/partials/LogoCarousel.jsx
@@ -1,16 +1,21 @@
-import RenderLogos from "../pages/RenderLogos";
-
 function LogoCarousel({ shops }) {
-  const [indiceActual, setIndiceActual] = usestate(0);
-  // const forward = ()=>{
-  //   setIndiceActual((indiceActual +1)% )
-  // }
+  function RenderLogos(shops) {
+    return shops.map((shop) => {
+      return (
+        <a href={`#shop-${shop.id_shop}`}>
+          <img
+            className="logos__carousel__item"
+            src={`/logos/${shop.logo_url}`}
+            alt={shop.name}
+          />
+        </a>
+      );
+    });
+  }
   return (
     <div className="logos__carousel">
       <div className="logos__carousel__scrollable">
-        <div className="logos__carousel__list">
-          <RenderLogos shops={shops} />
-        </div>
+        <div className="logos__carousel__list">{RenderLogos(shops)}</div>
       </div>
       <div className="logos__carousel__nav">
         <button className="logos__carousel__nav__control">
