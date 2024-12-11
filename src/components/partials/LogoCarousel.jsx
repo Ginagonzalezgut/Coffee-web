988102169597b83b917389de63d5972dadc1b457
Gina González <gini_go@hotmail.com>
2024-12-11 14:14:41 +0100

    fix styles to carousel

--- src/components/partials/LogoCarousel.jsx
+++ src/components/partials/LogoCarousel.jsx
@@ -5,7 +5,7 @@ import "../../scss/components/LogoCarousel.scss";
 
 function LogoCarousel({ shops }) {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
